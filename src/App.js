3921928d42fd132dc7de348136eacf3ceadd57ca
Andrei <andrei_zhigachev@bk.ru>
2023-08-03 00:24:12 +0300

    index.js содержит роутинг, App.js приложение

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { useState } from 'react';
+import useYandexOAuth from './hooks/useYandexOAuth';
+import YandexDiskService from './api/yandexDiskService';
+
 import './App.css';
 
 function App() {
+
+  const [token, error] = useYandexOAuth(process.env.REACT_APP_CLIENT_ID, process.env.REACT_APP_CLIENT_SECRET);
+  const [files, setFiles] = useState([]);
+
+  console.log(process.env.REACT_APP_CLIENT_ID, process.env.REACT_APP_CLIENT_SECRET)
+
+  const handleChangeFile = (e) => {
+    let inputFiles = e.target.files;
+    let arr = [];
+
+    for (let file of inputFiles) {
+      arr.push(file)
+    }
+    
+    setFiles(state => arr)
+  }
+
+  const handleClickUpload = () => {
+    YandexDiskService.uploadFiles(files, token)
+  }
+
   return (
     <div className="App">
-      
+      <input type="file" onChange={handleChangeFile} multiple />
+      <button onClick={handleClickUpload}>Загрузить</button>
     </div>
   );
 }
